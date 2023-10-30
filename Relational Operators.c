bf786c615e404221f71f0f576fe8f7fe39dbb638
Farzaan-Rather <147377642+Farzaan-Rather@users.noreply.github.com>
2023-10-30 18:12:12 +0530

    Add files via upload

---
+++ Relational Operators.c
@@ -0,0 +1,31 @@
+//Relational Operators
+//Take two variables and perform all the relational operators.
+//among those two Variables
+
+
+
+#include <stdio.h>
+int main () {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a>b);
+    printf("%d\n",a<b);
+    printf("%d\n",a=b);
+    printf("%d\n",a>=b);
+    printf("%d\n",a<=b);
+    printf("%d\n",a!=b);
+
+
+
+
+
+    return 0;
+
+
+
+
+
+
+
+
+}
