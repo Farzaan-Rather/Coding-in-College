b9b34f828f314329d43a73668390642bd7d46cdd
Farzaan-Rather <147377642+Farzaan-Rather@users.noreply.github.com>
2023-10-30 18:14:38 +0530

    Add files via upload

---
+++ Program for Even and Odd Numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main () {
+
+int a;
+printf("enter a");
+scanf("%d",&a);
+
+if (a%2==0) {
+
+    printf("Number is Even");
+}
+else {
+    printf("Number is Odd");
+}
+
+
+return 0;
+
+}
+
