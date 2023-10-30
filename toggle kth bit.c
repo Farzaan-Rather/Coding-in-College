75a59588f70fe4260c95cff61691732fd6060d2a
Farzaan-Rather <147377642+Farzaan-Rather@users.noreply.github.com>
2023-10-30 18:17:54 +0530

    Add files via upload

---
+++ toggle kth bit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+
+int number;
+printf("Enter any Number \n");
+scanf("%d",&number);
+int a= number<<1;
+printf("After un setting the right most bit is %d", a);
+return 0;
+
+
+
+
+}
